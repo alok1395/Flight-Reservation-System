b29460fe939846a4f21bead59287fbe3358c38ea
Alok Yadav <alokyadav95@gmail.com>
2020-11-23 15:39:09 +0530

    chackpoint 4

--- src/com/company/RegularTicket.java
+++ src/com/company/RegularTicket.java
@@ -1,4 +1,26 @@
 package com.company;
 
-public class RegularTicket {
+public class RegularTicket extends  Ticket{
+
+    private  String specialServices;
+
+    public String getSpecialServices() {
+        return specialServices;
+    }
+
+    public void setSpecialServices(String specialServices) {
+        this.specialServices = specialServices;
+    }
+
+    public RegularTicket(String pnr, String from, String t0, Flight flight, String departureDateTime, String arrivalDateTime, Passenger passenger, String seatNo, float price, boolean cancelled) {
+        super(pnr, from, t0, flight, departureDateTime, arrivalDateTime, passenger, seatNo, price, cancelled);
+    }
+
+
+    public String getSpecialServicesByFlight(){
+        return " ";
+    }
+    public  void updateSpecialServices(String specialServices){
+
+    }
 }
--- src/com/company/TouristTicket.java
+++ src/com/company/TouristTicket.java
@@ -1,4 +1,28 @@
 package com.company;
 
-public class TouristTicket {
+public class TouristTicket extends  Ticket{
+
+    String hotelAddress ;
+    String[] selectedTouristLocation = new String[5];
+
+    public TouristTicket(String pnr, String from, String t0, Flight flight, String departureDateTime,
+                         String arrivalDateTime, Passenger passenger, String seatNo,
+                         float price, boolean cancelled) {
+        super(pnr, from, t0, flight, departureDateTime, arrivalDateTime, passenger, seatNo, price, cancelled);
+    }
+
+
+    public String getHotelAddress() {
+        return hotelAddress;
+    }
+    public String[] getTouristLocation(){
+        return selectedTouristLocation;
+    }
+    public void removeTouristLocation(String location) {
+
+            }
+    public void addTouristLocation(String location){
+
+    }
+
 }
